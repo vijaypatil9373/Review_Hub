cc0d6b93b2115da3c4f2f2b127924dad433dea37
Vijay Patil <vijaygpatil9373@gmail.com>
2026-05-07 02:14:29 +0530

    Update app.py

--- Reviews_Project/app.py
+++ Reviews_Project/app.py
@@ -15,7 +15,15 @@ app.secret_key = "secret123"
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # ================= DATABASE =================
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+# ================= DATABASE =================
+
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
