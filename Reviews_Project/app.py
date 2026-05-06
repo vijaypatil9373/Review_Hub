0148b237015648118aaa195bb084d27dbd57c4af
Vijay Patil <vijaygpatil9373@gmail.com>
2026-05-07 02:08:32 +0530

    Update app.py

--- Reviews_Project/app.py
+++ Reviews_Project/app.py
@@ -15,9 +15,7 @@ app.secret_key = "secret123"
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # ================= DATABASE =================
-db_path = os.path.join(BASE_DIR, "users.db")
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_path
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
